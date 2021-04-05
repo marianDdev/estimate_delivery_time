75b00a2772dbe0901c56560700953834b7d7eb98
Marian Dumitru <d.mariandev@gmail.com>
2021-04-05 22:12:17 +0300

    table name correction inside donwn() method of delivery_dates migration

--- estimate_delivery_time/database/migrations/2021_04_05_150746_create_delivery_dates_table.php
+++ estimate_delivery_time/database/migrations/2021_04_05_150746_create_delivery_dates_table.php
@@ -28,6 +28,6 @@ class CreateDeliveryDatesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('deliveries');
+        Schema::dropIfExists('delivery_dates');
     }
 }
