673a1bb6168541ae39db227f8915007184181dac
Marian Dumitru <d.mariandev@gmail.com>
2021-04-06 01:48:52 +0300

    -created single action invokable controller and route -it handles the input request(the zip code) -the response is a json response(the delivery estimated time)

--- estimate_delivery_time/app/Http/Controllers/DeliveryController.php
+++ estimate_delivery_time/app/Http/Controllers/DeliveryController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use App\Services\DeliveryService;
 
 class DeliveryController extends Controller
 {
-    //
+    private $request;
+    private $response;
+    private $deliveryService;
+
+    public function __construct(
+        Request $request,
+        ResponseFactory $response,
+        DeliveryService $deliveryService
+    )
+    {
+        $this->request = $request;
+        $this->response = $response;
+        $this->deliveryService = $deliveryService;
+    }
+
+    public function __invoke(): JsonResponse
+    {
+        return $this->response->json($this->deliveryService->estimateDeliveryTime($this->request->input()));
+    }
 }
