4bdaaaf302416e18a8b22a79188ceaf5836eda91
Marian Dumitru <d.mariandev@gmail.com>
2021-04-06 12:40:57 +0300

    added unit test for model not found exception

--- estimate_delivery_time/tests/Unit/DeliveryTimeTest.php
+++ estimate_delivery_time/tests/Unit/DeliveryTimeTest.php
@@ -5,15 +5,17 @@ namespace Tests\Unit;
 use App\DeliveryRepository;
 use App\Services\DeliveryService;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Mockery;
 use Tests\TestCase;
 
 class DeliveryTimeTest extends TestCase
 {
-    public function test_can_estimate_delivery_time(): void
+    public function setUp(): void
     {
+        parent::setUp();
         $deliveryRepository = Mockery::mock(DeliveryRepository::class);
-        $deliveryRepository->shouldReceive('getDeliveryTimeDates')->with(96708)
+        $deliveryRepository->shouldReceive('getDeliveryTimeDates')->with("96708")
                            ->andReturn(collect([
                                                    [
                                                        "shipment_date" => "2021-04-08 22:44:44",
@@ -25,14 +27,32 @@ class DeliveryTimeTest extends TestCase
                                                    ]
                                                ]));
 
+        $deliveryRepository->shouldReceive('getDeliveryTimeDates')->with("10000")
+                           ->andThrow(ModelNotFoundException::class);
 
+    }
 
-        $input = ["zip_code" => 96708];
+    public function test_can_estimate_delivery_time(): void
+    {
+
         $expected = Carbon::now()->addDays(8)->toDateTimeString();
 
         /** @var DeliveryService $deliveryService */
         $deliveryService = $this->app->get(DeliveryService::class);
-        $actual = $deliveryService->estimateDeliveryTime($input);
+        $actual = $deliveryService->estimateDeliveryTime("96708");
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function test_can_not_find_zip_code() {
+
+        //$this->expectException(ModelNotFoundException::class);
+
+        $expected = "Zip Code not found";
+
+        /** @var DeliveryService $deliveryService */
+        $deliveryService = $this->app->get(DeliveryService::class);
+        $actual = $deliveryService->estimateDeliveryTime("10000");
 
         $this->assertEquals($expected, $actual);
     }
